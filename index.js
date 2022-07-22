3c64243ec2f9895acafd285b745f066a8203968a
JokeDevil <49778541+JokeDevil@users.noreply.github.com>
2022-07-22 09:25:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ for (const x in config.tokens) {
         partials: ["CHANNEL"],
     });
     if(config.tokens[x].guildID){
-        await console.log(`^1JD_logsV3 Error:^0 Bot ^4${x}^0 is missing a GuildID in de config.`)
-        await console.log(`^1JD_logsV3 Error:^0 Make sure to add this to your config or the bot won't send any messages.`) 
+        console.log(`^1JD_logsV3 Error:^0 Bot ^4${x}^0 is missing a GuildID in de config.`)
+        console.log(`^1JD_logsV3 Error:^0 Make sure to add this to your config or the bot won't send any messages.`) 
     }
     if(config.tokens[x].token !== ''){
         client[x].login(config.tokens[x].token).catch(async err => {
